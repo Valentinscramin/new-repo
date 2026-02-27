23b256d9c91c4d3c54a9fcdb401db16ad593629a
IDK MY NAME <Valentinscramin@users.noreply.github.com>
2026-02-27 23:34:13 +0000

    REMOVE

--- portal-tech/config/bundles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
-];
